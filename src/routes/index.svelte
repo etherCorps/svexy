59224cfed84f0c64ec0f99a27df45a06bea11e06
Shivam Meena <meenashivam9650@gmail.com>
2022-06-28 00:00:09 +0530

    Example addition

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,8 @@
 <script>
+import { Button } from "svexy-ui";
 </script>
 
 <div class="h-screen bg-gray-200">
-<!-- <Button label="Shivam" class="m-4 text-2xl rounded-3xl hover:bg-indigo-800 bg-indigo-200" outline/>-->
-<!-- <Button label="Shivam" class="m-4 text-2xl rounded-3xl hover:bg-red-600 bg-red-600 ease-linear" filled/>-->
+ <Button label="Shivam" class="m-4 text-2xl rounded-3xl hover:bg-indigo-800 bg-indigo-200" outline/>
 </div>
 
